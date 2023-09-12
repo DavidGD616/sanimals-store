e4f537351ca326fbee4dc71209cd5a55eb3e537d
DavidGD616 <davidguerrerodiaz12@gmail.com>
2023-09-11 21:50:09 -0700

    Header arrows down

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,20 @@
+import ArrowDown from '../svg/ArrowDown.svg'
+
 function Menu() {
     return (
         <ul className="header__ul">
-            <li className="header__ul-li">New in</li>
-            <li className="header__ul-li">Accesories</li>
-            <li className="header__ul-li">Collections</li>
+            <li className="header__ul-li">
+                New in
+                <img className='header__ul-li-arrowdown' src={ArrowDown} alt='ArrowDown' />
+            </li>
+            <li className="header__ul-li">
+                Accesories
+                <img className='header__ul-li-arrowdown' src={ArrowDown} alt='ArrowDown' />
+                </li>
+            <li className="header__ul-li">
+                Collections
+                <img className='header__ul-li-arrowdown' src={ArrowDown} alt='ArrowDown' />
+                </li>
         </ul>
     )
 }
