616e4f8c04184a1bbba7d6e2bb1fe0465bbc925f
DavidGD616 <davidguerrerodiaz12@gmail.com>
2024-02-20 18:18:08 -0800

    Few details fixed

--- src/components/BannerContent.js
+++ src/components/BannerContent.js
@@ -3,7 +3,14 @@ function BannerContent () {
         <div className="banner__content">
             <h1 className="banner__content-h1">Feel Yourself Like an Animal</h1>
             <p className="banner__content-p">Make an animal part of your daily look!</p>
-            <button className="banner__content-button">Shop Now</button>
+            <button className="banner__content-button">
+                Shop Now
+                <svg width="20" height="20" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="0.5" y="0.502441" width="27" height="27" stroke="#56D1DC"/>
+<path d="M8.75 5.25244V7.00244H19.7663L5.25 21.5187L6.48375 22.7524L21 8.23619V19.2524H22.75V5.25244H8.75Z" fill="#ECECEE"/>
+</svg>
+
+            </button>
         </div>
     )
 }
