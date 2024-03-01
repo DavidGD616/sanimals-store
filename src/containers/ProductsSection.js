a595a510cdb8f7c40ef3a0dbe7efaa479c6bbb51
DavidGD616 <davidguerrerodiaz12@gmail.com>
2024-02-29 21:27:02 -0800

    ProductCard

--- src/containers/ProductsSection.js
+++ src/containers/ProductsSection.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { ProductCard } from "../components/ProductPage/ProductCard"; 
 
 
 function ProductsSection({ products }) {
@@ -7,33 +7,7 @@ function ProductsSection({ products }) {
         <>
         <h1 className="text-4xl font-bold text-center my-12">All Products</h1>
         <div className="mt-10 grid grid-cols-2 gap-6 lg:mt-16 lg:grid-cols-4 lg:gap-4">
-            {products.map((product, index) => (
-                <div className="group my-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md">
-                    <Link to='#' className="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl">
-                        <img className="peer absolute top-0 right-0 h-full w-full object-cover"
-                        src={product.images[1]} alt={product.title} />
-                        <img className="peer absolute top-0 -right-96 h-full w-full object-cover transition-all delay-100 duration-1000 hover:right-0 peer-hover:right-0"
-                        src={product.images[0]} alt={product.images} />
-                        <svg class="pointer-events-none absolute inset-x-0 bottom-5 mx-auto text-3xl text-white  transition-opacity group-hover:animate-ping group-hover:opacity-30 peer-hover:opacity-0" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32"><path fill="currentColor" d="M2 10a4 4 0 0 1 4-4h20a4 4 0 0 1 4 4v10a4 4 0 0 1-2.328 3.635a2.996 2.996 0 0 0-.55-.756l-8-8A3 3 0 0 0 14 17v7H6a4 4 0 0 1-4-4V10Zm14 19a1 1 0 0 0 1.8.6l2.7-3.6H25a1 1 0 0 0 .707-1.707l-8-8A1 1 0 0 0 16 17v12Z" /></svg>
-                    </Link>
-                    <div className="mt-4 px-5 pb-5">
-                        <Link to='#'>
-                            <h5 className="sm:text-sm md:text-xl text-xl tracking-tight text-slate-900">{product.title}</h5>
-                        </Link>
-                        <div className="mt-2 mb-5 flex sm:flex-col md:flex-row flex-col items-center justify-between ">
-                            <p>
-                                <span className="text-[20px]   font-bold text-slate-900">${product.price}</span>
-                            </p>
-                            <Link to='#' className="mt-2 flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-                                </svg>
-                                Add to cart
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-            ))}
+            <ProductCard products={products} />
         </div>
         </>
     )
